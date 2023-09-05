a7e51b82f04be10ae5d1bd1341c0d266a5c4d74d
toniles <120176462+toniles@users.noreply.github.com>
2023-09-05 11:31:11 +0200

    Update tool.sh
    fixed copying archives, created a func

--- tool.sh
+++ tool.sh
@@ -16,6 +16,26 @@ WIN_ISO="/home/ticot/Descargas/Win10_22H2_Spanish_x64v1.iso"
 FEDORA_ISO="/home/ticot/Descargas/Fedora-Workstation-Live-x86_64-38-1.6.iso"
 LOGOPEDIA="/home/ticot/Documentos/Logopedia.zip"
 
+safe_copy() {
+    SOURCE=$1
+    DEST=$2
+    REQUIRED_SPACE=$(du -s $SOURCE | cut -f1)
+    AVAILABLE_SPACE=$(df $DEST | tail -1 | awk '{print $4}')
+    
+    if [[ $REQUIRED_SPACE -gt $AVAILABLE_SPACE ]]; then
+        log "No hay suficiente espacio en $DEST para copiar $SOURCE. Espacio necesario: $REQUIRED_SPACE, espacio disponible: $AVAILABLE_SPACE."
+        exit 1
+    fi
+
+    rsync -ah --no-owner --no-group --progress $SOURCE $DEST || {
+        log "Error usando rsync para copiar $SOURCE a $DEST. Intentando con cp..."
+        cp -r $SOURCE $DEST || {
+            log "Error copiando $SOURCE a $DEST con cp. Abortando."
+            exit 1
+        }
+    }
+}
+
 # Función mejorada para desmontar particiones
 unmount_partitions() {
     for partition in "$1"*; do
@@ -43,8 +63,8 @@ unmount_partitions $USB_FEDORA
 wipefs -a $USB_FEDORA || { log "Error limpiando $USB_FEDORA"; exit 1; }
 
 log "Creando particiones..."
-echo -e ",10G,*\n," | sfdisk $USB_WIN || { log "Error particionando $USB_WIN"; exit 1; }
-echo -e ",10G,*\n," | sfdisk $USB_FEDORA || { log "Error particionando $USB_FEDORA"; exit 1; }
+echo -e ",10G,*\\n," | sfdisk $USB_WIN || { log "Error particionando $USB_WIN"; exit 1; }
+echo -e ",10G,*\\n," | sfdisk $USB_FEDORA || { log "Error particionando $USB_FEDORA"; exit 1; }
 
 log "Formateando particiones..."
 mkfs.fat -F 32 ${USB_WIN}1 || { log "Error formateando ${USB_WIN}1"; exit 1; }
@@ -60,8 +80,8 @@ mount ${USB_WIN}2 /mnt/win_data || { log "Error montando ${USB_WIN}2"; exit 1; }
 mount ${USB_FEDORA}2 /mnt/fedora_data || { log "Error montando ${USB_FEDORA}2"; exit 1; }
 
 log "Copiando archivos..."
-rsync -ah --progress $LOGOPEDIA /mnt/win_data || { log "Error copiando $LOGOPEDIA a /mnt/win_data"; exit 1; }
-rsync -ah --progress --exclude='Logopedia.zip' /home/ticot/Documentos/ /mnt/fedora_data/ || { log "Error copiando documentos a /mnt/fedora_data/"; exit 1; }
+safe_copy /home/ticot/Documentos/ /mnt/fedora_data/
+safe_copy $LOGOPEDIA /mnt/win_data
 
 unmount_partitions $USB_WIN
 unmount_partitions $USB_FEDORA
@@ -71,4 +91,3 @@ woeusb --target-filesystem NTFS --device $WIN_ISO ${USB_WIN}1 || { log "Error cr
 dd if=$FEDORA_ISO of=${USB_FEDORA}1 bs=4M status=progress oflag=sync || { log "Error creando USB de arranque con $FEDORA_ISO"; exit 1; }
 
 log "Listo! Ambos USBs están preparados."
-
